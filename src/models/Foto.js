07a26bdf985c92474f87e93346661a26b6ec98fe
Chagas011 <washington.chagas.9@hotmail.com>
2024-03-16 12:51:30 -0300

    alterando url foto

--- src/models/Foto.js
+++ src/models/Foto.js
@@ -1,6 +1,6 @@
 import Sequelize, { Model } from 'sequelize';
 
-const url = 'http://localhost:3000';
+const url = 'http://192.168.15.124';
 export default class Foto extends Model {
   static init(sequelize) {
     super.init({
