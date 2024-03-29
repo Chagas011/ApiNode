72a7d21b768cbd732ad4d8f3d4cd5b9fee139586
Chagas011 <washington.chagas.9@hotmail.com>
2024-03-29 11:04:39 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import { resolve } from 'path';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
@@ -17,6 +19,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
